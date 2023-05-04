97ccf24d048e805cb00ba8c9138d1ea38d727f95
KleinYuan <kaiwen.yuan1992@gmail.com>
2023-05-04 01:33:23 -0700

    add by accident commented out model download

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
-#wget -P ckp/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+wget -P ckp/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+# uncomment below to download more models
 #wget -P ckp/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
 #wget -P ckp/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
